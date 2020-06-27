ed69f0a6b5fce81941e419adf3af94ec5c107776
govl0407 <govl0407@gmail.com>
2020-06-28 01:48:01 +0900

    add file

---
+++ template.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+        <body>
+                <form action="">
+                        number =  <input type="number" name="a">
+                               ,  <input type="number" name="b">
+                        <input type="submit">
+                </form>
+                <iframe src="/calc/cal.txt" "width"1000px" height = "300px" ></iframe>
+        </body>
+</html>
+"""
+
