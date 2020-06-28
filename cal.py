5f3f0fd36643a12beeb351f882c02f2f3c42a459
govl0407 <govl0407@gmail.com>
2020-06-28 13:09:00 +0900

    if no input

--- cal.py
+++ cal.py
@@ -6,6 +6,9 @@ def application(environ, start_response):
         d = parse_qs(environ['QUERY_STRING'])
         a = d.get('a', [''])[0]
         b = d.get('b', [''])[0]
+        f =open("/usr/local/calc/cal.txt", "w")
+        f.write("no input")
+        f.close()
         if '' not in [a,b]:
                 a,b= int(a), int(b)
                 x = [a+b]
@@ -18,9 +21,12 @@ def application(environ, start_response):
                 f.close()
 
         response_body = html
+
         start_response('200 OK', [
                 ('Content-Type', 'text/html'),
                 ('Content-Length', str(len(response_body)))
         ])
+
+
         return [response_body]
 
